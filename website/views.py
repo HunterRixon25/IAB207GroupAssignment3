965177832d7aa0794db5160c73ad10e1f84fb44b
Zach Andrews <zachary.andrews@connect.qut.edu.au>
2023-10-05 18:46:56 +1000

    Updated files, ready for further work

--- website/views.py
+++ website/views.py
@@ -1,8 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for
+from .models import Events
+from . import db
 
-bp = Blueprint('main', __name__)
+mainbp = Blueprint('main', __name__)
 
-
-@bp.route('/')
+@mainbp.route('/')
 def index():
-    return '<h1>Starter code for the assessment<h1>'
+    event = db.session.scalars(db.select(Events)).all()    
+    return render_template('index.html', event=event)
+
+@mainbp.route('/search')
+def search():
+    if request.args['search'] and request.args['search'] != "":
+        print(request.args['search'])
+        query = "%" + request.args['search'] + "%"
+        event = db.session.scalars(db.select(Events).where(Events.description.like(query)))
+        return render_template('index.html', events=event)
+    else:
+        return redirect(url_for('main.index'))
