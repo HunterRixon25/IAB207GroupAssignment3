e47efc730931a6ddea92b944f9f6b43e1d4e4df7
Harry Wright <145808614+HunterRixon25@users.noreply.github.com>
2023-10-26 11:26:37 +1100

    Update forms.py
    Added EditForm to default forms

--- website/forms.py
+++ website/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField, SelectField, IntegerField, FloatField, DateField, TimeField
+from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField, SelectField, IntegerField, FloatField, DateField, TimeField, BooleanField
 from wtforms.validators import InputRequired, Email, EqualTo, NumberRange
 from flask_wtf.file import FileRequired, FileField, FileAllowed
+from .models import EventState
 
 ALLOWED_FILE = {'PNG', 'JPG', 'JPEG', 'png', 'jpg', 'jpeg'}
 
@@ -25,6 +26,27 @@ class EventsForm(FlaskForm):
   eventDate = DateField('Event Date', validators=[InputRequired()])
   eventTime = TimeField('Event Time', validators=[InputRequired()])
   submit = SubmitField("Create Event")
+
+class EditForm(FlaskForm):
+  name = StringField('Event Name', validators=[InputRequired()])
+  # eventCategory = StringField('Event Category', validators=[InputRequired()])
+  eventCategory = SelectField('Event Category', choices=[('', 'Select Category'), ('Blues', 'Blues'), ('Bebop', 'Bebop'), ('Soul', 'Soul')])
+  cancelEvent = SelectField('Change Event Status', choices=[(EventState.OPEN.value, EventState.OPEN.value), (EventState.CANCELLED.value, EventState.CANCELLED.value)])
+  description = TextAreaField('Description', 
+            validators=[InputRequired()])
+  image = FileField('Destination Image', validators=[
+    FileAllowed(ALLOWED_FILE, message='Only supports PNG, JPG, png, jpg')])
+  ticketCapacity = IntegerField('Number of Tickets', validators=[InputRequired(), NumberRange(min=1)])
+  ticketPrice = FloatField('Ticket Price', validators=[InputRequired()])
+  address1 = StringField('Address 1', validators=[InputRequired()])
+  address2 = StringField('Address 2')
+  city = StringField('City', validators=[InputRequired()])
+  state = SelectField('State', choices=[('', 'Select State'), ('Queensland', 'Queensland'), ('New South Wales', 'New South Wales'), ('Victoria', 'Victoria'), ('Western Australia', 'Western Australia'), ('South Australia', 'South Australia'), ('Tasmania', 'Tasmania')])
+  postcode = StringField('Postcode', validators=[InputRequired()])
+  venueCapacity = IntegerField('Total Venue Capacity', validators=[InputRequired(), NumberRange(min=1)])
+  eventDate = DateField('Event Date', validators=[InputRequired()])
+  eventTime = TimeField('Event Time', validators=[InputRequired()])
+  submit = SubmitField("Update Event")
     
 class LoginForm(FlaskForm):
     user_name = StringField("User Name", validators=[InputRequired('Enter user name')])
